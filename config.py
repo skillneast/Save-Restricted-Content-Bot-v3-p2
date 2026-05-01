29ee0b4e41fcad2799641be9cc62a1dbd8408ea5
skillneast <allworkwithfahad@gmail.com>
2026-05-02 00:17:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,8 +45,8 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT  = int(os.getenv("PREMIUM_LIMIT", "500"))
 
 # ─── UI / LINKS ─────────────────────────────────────────────────────────────────
-JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro")
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+JOIN_LINK     = os.getenv("JOIN_LINK", "https://t.me/@skillneast")
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/Voidd_zy")
 
 # ════════════════════════════════════════════════════════════════════════════════
 # ░ PREMIUM PLANS CONFIGURATION
